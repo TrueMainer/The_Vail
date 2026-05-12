86a65abbe8cd68235e107cff7b3fecea9159f15e
TrueMainer <jamesbondedc4@gmail.com>
2026-05-12 14:04:32 -0400

    Initial working NeoForge setup

--- src/main/java/com/truemainer/thevail/Config.java
+++ src/main/java/com/truemainer/thevail/Config.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.truemainer.thevail;
 
 import java.util.List;
 import java.util.Set;
--- src/main/java/com/truemainer/thevail/TheVail.java
+++ src/main/java/com/truemainer/thevail/TheVail.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.truemainer.thevail;
 
 import org.slf4j.Logger;
 
@@ -32,10 +32,10 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod {
+@Mod(TheVail.MODID)
+public class TheVail {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "thevail";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -65,7 +65,7 @@ public class ExampleMod {
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ExampleMod(IEventBus modEventBus, ModContainer modContainer) {
+    public TheVail(IEventBus modEventBus, ModContainer modContainer ) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -86,6 +86,8 @@ public class ExampleMod {
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+
+        LOGGER.info("The Veil has awakened.");
     }
 
     private void commonSetup(FMLCommonSetupEvent event) {
